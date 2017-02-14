b51cdf6841a98538ae5752f4c1660d52bcc5b2ea
justinhessdev <justinhessdev@gmail.com>
2017-02-14 14:04:51 -0800

    added some routes

--- server/app.js
+++ server/app.js
@@ -6,43 +6,60 @@ const
   cookieParser = require('cookie-parser'),
   bodyParser = require('body-parser')
 
-  // environment port
-  const
-      port = process.env.PORT || 3000,
-      mongoConnectionString = process.env.MONGODB_URL || 'mongodb://localhost/context-app'
+// environment port
+const
+    port = process.env.PORT || 3000,
+    mongoConnectionString = process.env.MONGODB_URL || 'mongodb://localhost/context-app'
 
-  const
-    userSchema = mongoose.Schema({
+
+var userSchema = new mongoose.Schema({
       name:String,
       number:String,
       email:String,
       password:String
-    }),
-    User = mongoose.model('User', userSchema)
-
-  mongoose.connect(mongoConnectionString, (err) => {
-    console.log(err || "Connected to MongoDB (context-app)")
   })
 
-  console.log(process.env.PWD)
+ var User = mongoose.model('User', userSchema)
 
-  // now app will look for everything in static client directory
-  // process.env finds the environment of our app .. and we can access the root (pwd)
-  app.use(express.static(process.env.PWD + '/client'))
+mongoose.connect(mongoConnectionString, (err) => {
+  console.log(err || "Connected to MongoDB (context-app)")
+})
 
-  // send res (response) back to client when client makes get request at root.
-  // response contains sendFile of our client index.html so client/chrome will know to populate page
-  app.get('/', (req, res) => {
-    res.sendFile(process.env.PWD + '/client/index.html')
-  })
+// now app will look for everything in static client directory
+// process.env finds the environment of our app .. and we can access the root (pwd)
+app.use(express.static(process.env.PWD + '/client'))
 
-  // API routes here: as opposed to our /#/ routes
-  // app.get('/api/users', (req, res) => {
-  //   User.find({}, (err, users) => {
-  //     res.json(users)
-  //   })
-  // })
+//////////////////
 
-  app.listen(port, (err) => {
-    console.log(err || "Server running")
-  })
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+// send res (response) back to client when client makes get request at root.
+// response contains sendFile of our client index.html so client/chrome will know to populate page
+app.get('/', (req, res) => {
+  res.sendFile(process.env.PWD + '/client/index.html')
+})
+
+// root / index
+app.get('/users', function(req, res) {
+    User.find({}, function(err, users) { // we put the find criteria in '{}' --> if empty we are looking for all
+      res.json(users)
+    })
+})
+
+app.post('/users', function(req, res) {
+  User.create(req.body, function(err, user) {
+    res.json(user)
+  }) // body belongs to body parser middleware
+})
+
+// API routes here: as opposed to our /#/ routes
+// app.get('/api/users', (req, res) => {
+//   User.find({}, (err, users) => {
+//     res.json(users)
+//   })
+// })
+
+app.listen(port, (err) => {
+  console.log(err || "Server running")
+})
