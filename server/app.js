2f1bc5c167953919a824dde8f0c440f18b793c4e
justinhessdev <justinhessdev@gmail.com>
2017-02-14 11:19:52 -0800

    added ejs template views for passport

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const
-  express = require('express'),
-  app = express(),
-  mongoose = require('mongoose'),
-  logger = require('morgan'),
-  cookieParser = require('cookie-parser'),
-  bodyParser = require('body-parser')
+    express = require('express'),
+    app = express(),
+    ejs = require('ejs'),
+    ejsLayouts = require('express-ejs-layouts'),
+    mongoose = require('mongoose'),
+    flash = require('connect-flash'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    MongoDBStore = require('connect-mongodb-session')(session),
+    passport = require('passport')
 
   // environment port
   const
@@ -24,16 +30,37 @@ const
     console.log(err || "Connected to MongoDB (context-app)")
   })
 
-  console.log(process.env.PWD)
+  // will store session information as a 'sessions' collection in Mongo
+const store = new MongoDBStore({
+  uri: mongoConnectionString,
+  collection: 'sessions'
+});
 
-  // now app will look for everything in static client directory
-  // process.env finds the environment of our app .. and we can access the root (pwd)
-  app.use(express.static(process.env.PWD + '/client'))
+// middleware
+
+// now app will look for everything in static client directory
+// process.env finds the environment of our app .. and we can access the root (pwd)
+app.use(express.static(process.env.PWD + '/client'))
+app.use(logger('dev'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(flash())
+
+// ejs configuration
+app.set('view engine', 'ejs')
+app.set('views', process.env.PWD+'/client/views');
+app.use(ejsLayouts)
+
 
   // send res (response) back to client when client makes get request at root.
   // response contains sendFile of our client index.html so client/chrome will know to populate page
+  // app.get('/', (req, res) => {
+  //   res.render('/client/views/index')
+  // })
+
   app.get('/', (req, res) => {
-    res.sendFile(process.env.PWD + '/client/index.html')
+    res.render('index')
   })
 
   // API routes here: as opposed to our /#/ routes
