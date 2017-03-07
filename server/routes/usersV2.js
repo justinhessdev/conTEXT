a17a9ff2b4bb17618b709676ed279715de3fa134
justinhessdev <justinhessdev@gmail.com>
2017-03-07 17:19:37 +0200

    fixed bug removed passport auth for get userV2 routes

--- server/routes/usersV2.js
+++ server/routes/usersV2.js
@@ -1,31 +1,22 @@
 // /routes/users.js
 const
     express = require('express'),
-    passport = require('passport'),
     userV2Router = express.Router(),
     User = require('../models/User.js')
 
-
 ///////////////////////////////////////////
-userV2Router.get('/', isLoggedIn, (req, res) => {
+userV2Router.get('/', (req, res) => {
   User.find({}, (err, users) => {
     console.log(users)
     res.json(users)
   })
 })
 
-userV2Router.route('/:id')
-  .get((req,res) => {
+userV2Router.get('/:id', (req,res) => {
     User.findById(req.params.id , (err, user) =>{
       if (err) console.log(err)
       res.json(user)
     })
   })
-
-// a method used to authorize a user BEFORE allowing them to proceed to the profile page:
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()) return next()
-    res.redirect('/')
-}
 
 module.exports = userV2Router
